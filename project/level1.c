4d3835fdaa804c0e24c3b2685c9cfa0f460cc4d0
Alex Melendez <ammelendez4@miners.utep.edu>
2017-12-07 00:06:43 -0700

    stuff

--- project/level1.c
+++ project/level1.c
@@ -190,6 +190,39 @@ void start1()
   }
 }
 
+void wdt_c_handler()
+{
+  static short count = 0;
+  P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
+  count ++;
+  if(menu){
+  //   if(str[0]){
+  //     menu = 0;
+  //     lvl1 = 1;
+  //     start1();
+  //   }
+  //   if(str[1])
+  //   controlInstructions();
+  // }
+  if(lvl1){
+    if (count%15 == 0) {
+      if(count%240 == 0){
+        timer--;
+        sprintf(time_text, "%02d", timer);
+        drawString5x7(60,5,time_text,COLOR_WHITE,COLOR_BLACK);
+      }
+      collisionCheck(&ml0, &ml1);
+      collisionCheck(&ml1, &ml0);
+      if(collisionCheck(&ml0, &mlp) || collisionCheck(&ml1, &mlp)|| collisionCheck(&mlp,&ml0) || collisionCheck(&mlp,&ml1)){
+        endGame();
+      }
+      mlAdvance(&mlp, &fieldFence);
+      gameSwitchCheck();
+    }
+  }
+  P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
+}
+
 void gameSwitchCheck(){
   if(str[0]){
 	  mlp.velocity.axes[0] = -5;
@@ -213,6 +246,7 @@ void gameSwitchCheck(){
 	}
 	if (p2sw_read())
 	  redrawScreen = 1;
+  count = 0;
 }
 
 void endGame(){
