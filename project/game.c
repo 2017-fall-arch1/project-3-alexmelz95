e83e2fc238be667e11b9c3c8ce956424a6e80623
Alex Melendez <ammelendez4@miners.utep.edu>
2017-12-07 04:04:04 -0700

    stuff

--- project/game.c
+++ project/game.c
@@ -219,7 +219,7 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
       collisionCheck(&ml0, &ml1);
       collisionCheck(&ml1, &ml0);
       if(collisionCheck(&ml0, &mlp) || collisionCheck(&ml1, &mlp) || collisionCheck(&mlp,&ml0) || collisionCheck(&mlp,&ml1)){
-        buzzer_set_period(450);
+        buzzer_set_period(880);
         endGame = 1;
         // lose();
       }
